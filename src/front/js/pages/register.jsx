d60b163534f15fd1102d5255b1359b8cb03d3ca4
Billy Storm <billstormbiz@gmail.com>
2024-08-04 16:00:00 +0000

    Created token with fetch

--- src/front/js/pages/register.jsx
+++ src/front/js/pages/register.jsx
@@ -38,16 +38,16 @@ const Register = () => {
   };
 
   return (
-    <section className="vh-100">
+    <section className="vh-100 mt-4">
       <div className="container h-100 ">
-        <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="row d-flex justify-content-center align-items-center h-90">
           <div className="col-lg-12 col-xl-11">
             <div className="card text-black" style={{ borderRadius: "25px" }}>
               <div className="card-body p-md-5">
                 <div className="row justify-content-center">
                   <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                     <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                      Sign up
+                      Create Your Account
                     </p>
                     <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
                       <div className="d-flex flex-row align-items-center mb-4">
